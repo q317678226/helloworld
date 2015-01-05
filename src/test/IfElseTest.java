091c1ddf58193eec45f0a10082244316f9a17c16
赵文周 <317678226@qq.com>
2015-01-05 10:38:48 +0800

    测试if else

---
+++ src/test/IfElseTest.java
@@ -0,0 +1,14 @@
+package test;
+
+public class IfElseTest {
+	public static void main(String[] args) {
+		int a = 10;
+		if (a==10) {
+			System.out.println("a="+a);
+		}else if (a==9) {
+			System.out.println("a="+a);
+		}else {
+			System.out.println("a="+a);
+		}
+	}
+}
